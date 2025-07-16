cf493ce1704ff7c312032ed265007e671ff9a2ed
maulidiana17 <maulidiabwi77@gmail.com>
2025-07-16 13:13:10 +0700

    fix auth

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
         ];
 
         if(Auth::attempt($data)){
-            return redirect()->route('dashboard');
+            return redirect('/dashboard');
         } else{
             return redirect()->route('login')->with('failed','email atau password salah');
         }
