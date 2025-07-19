ba28449ab8cf8626b397635e11c401b9517539a8
agustinaardini <agustinaardinilestari@gmail.com>
2025-07-19 17:30:46 +0700

    add fix responsive

--- resources/views/absensi/rekap.blade.php
+++ resources/views/absensi/rekap.blade.php
@@ -63,7 +63,7 @@
                                     </div>
                                 </div>
                                 <div class="row mt-2">
-                                    <div class="col-6">
+                                     <div class="col-12 col-md-6 mb-2">
                                         <div class="form-group">
                                             <button type="submit" name="cetak" class="btn btn-primary w-100">
                                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-printer"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -74,7 +74,7 @@
                                             </button>
                                         </div>
                                     </div>
-                                    <div class="col-6">
+                                     <div class="col-12 col-md-6 mb-2">
                                         <div class="form-group">
                                             <button type="submit" name="exportexcel" class="btn btn-success w-100">
                                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-file-type-xls">
