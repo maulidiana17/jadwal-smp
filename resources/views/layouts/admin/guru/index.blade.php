86a025c56d1c3b41e47e5a2d399c0d64b08904c7
agustinaardini <agustinaardinilestari@gmail.com>
2025-07-26 04:25:00 +0700

    add responsive

--- resources/views/layouts/admin/guru/index.blade.php
+++ resources/views/layouts/admin/guru/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.admin.dashboard')
 
 @section('content')
-<div class="container-fluid">
+<div class="container">
     <div class="page-inner">
-        <div class="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center pt-3 pb-4">
+        <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
             <div>
-                <h3 class="fw-bold mb-1">Data Guru</h3>
-                <h6 class="text-muted mb-2">Daftar seluruh guru yang terdaftar</h6>
+                <h3 class="fw-bold mb-3">Data Guru</h3>
+                <h6 class="op-7 mb-2">Data Guru</h6>
 
                 @if(session('success'))
                     <div class="alert alert-success">
@@ -14,48 +14,64 @@
                     </div>
                 @endif
             </div>
-
         </div>
 
         <div class="row">
             <div class="col-12">
-                <div class="card shadow-sm">
+                <div class="card">
                     <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover mt-3">
-                                <thead class="table-dark">
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Kode Guru</th>
-                                        <th>Nama</th>
-                                        <th>NIP</th>
-                                        <th>Email</th>
-                                        <th>Alamat</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($gurus as $g)
-                                        <tr>
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td>{{ $g->kode_guru }}</td>
-                                            <td>{{ $g->nama }}</td>
-                                            <td>{{ $g->nip }}</td>
-                                            <td>{{ $g->email }}</td>
-                                            <td>{{ $g->alamat }}</td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                        <div class="row">
+                            <div class="col-12">
+
+                            </div>
                         </div>
 
-                        {{-- Pagination --}}
-                        <div class="d-flex justify-content-center mt-4">
-                            {{ $gurus->links('pagination::bootstrap-5') }}
+                        <div class="row mt-2 mb-2">
+                            <div class="col-12">
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-striped-bg-black mt-3 table-hover">
+                                        <thead>
+                                             <tr>
+                                                <th>No</th>
+                                                <th>Kode Guru</th>
+                                                <th>Nama</th>
+                                                <th>NIP</th>
+                                                <th>Email</th>
+                                                <th>Alamat</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                             @foreach ($gurus as $g)
+                                                <tr>
+                                                    <td>{{ $loop->iteration }}</td>
+                                                    <td>{{ $g->kode_guru }}</td>
+                                                    <td>{{ $g->nama }}</td>
+                                                    <td>{{ $g->nip }}</td>
+                                                    <td>{{ $g->email }}</td>
+                                                    <td>{{ $g->alamat }}</td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+
+                                       {{-- Pagination --}}
+                                    <div class="d-flex justify-content-center mt-3">
+                                        {{ $gurus->links('pagination::bootstrap-5') }}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+
+
+
 </div>
 @endsection
+
+
+
